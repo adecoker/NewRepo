dc133b411d085defab9ec3808acb2f18efacb1af
Ade Coker <88948587+adecoker@users.noreply.github.com>
2023-10-07 01:11:22 -0400

    Add files via upload
    new script uploaded

---
+++ addition.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+#Purpose: addition 
+#Version: 1.0
+#Created Date:  Fri Sep 1 03:20:30 UTC 2023
+#Modified Date: 
+#Authur: #Ade Coker
+# START #
+echo -e "Enter the first number : \c"
+read -r a
+echo -e "Enter the second number : \c"
+read -r b
+sum=$(($a+$b))
+echo "The sum is = $sum "
+# END #
